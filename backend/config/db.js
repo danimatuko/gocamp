d73b981a174e3880528b13e91979b266a945f08d
Dani Matuko <matuko305@gmail.com>
2021-06-13 13:43:19 +0300

    added custom error handler

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
-import { async } from "rxjs";
 
 const connectToDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
-			useUnifiedTopology: true
+			useUnifiedTopology: true,
+			useCreateIndex: true
 		});
-		console.log(`MongoDB connected: ${conn.connection.host}`);
+		console.log(`MongoDB connected: ${conn.connection.host}`.green);
 	} catch (error) {
-		console.error(error.message);
+		console.error(error.message.red);
 		process.exit(1);
 	}
 };
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 import express from "express";
-import products from "../backend/data/products.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 import dotenv from "dotenv";
 import connectToDB from "./config/db.js";
+import "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 connectToDB();
 
-app.get("/", (req, res) => {
-	res.send("api is running");
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-	res.json(products);
-});
+app.use(notFound);
 
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((product) => product._id === req.params.id);
-	res.json(product);
-});
+app.use(errorHandler);
 
-app.listen(PORT, console.log(`server listening in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+	PORT,
+	console.log(`server listening in ${process.env.NODE_ENV} mode on port ${PORT}`.green)
+);
