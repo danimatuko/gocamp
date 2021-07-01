6ad3ae9f168bc40f470436f553c2c1f86f75d3ad
Dani Matuko <matuko305@gmail.com>
2021-07-01 09:59:25 +0300

    create and update product endpoints

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -15,6 +15,45 @@ const getProductById = asyncHandler(async (req, res) => {
 	}
 });
 
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: "Sample Name",
+		price: 0,
+		user: req.user.id,
+		image: "/images.sample.jpg",
+		brand: "Sample Brand",
+		category: "Sample Category",
+		countInStock: 0,
+		numReviews: 0,
+		description: "Sample Decsription"
+	});
+
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+	const { name, price, image, brand, categorey, countInStock, decription } = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.image = image;
+		product.decription = decription;
+		product.brand = brand;
+		product.categorey = categorey;
+		product.countInStock = countInStock;
+
+		const updatedProduct = await product.save();
+		res.status(200).json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+});
+
 const deleteProduct = asyncHandler(async (req, res) => {
 	const product = await Product.findById(req.params.id);
 	if (product) {
@@ -26,4 +65,4 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	}
 });
 
-export { getProducts, getProductById, deleteProduct };
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,21 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById, deleteProduct } from "../controllers/productsController.js";
+import {
+	getProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct
+} from "../controllers/productsController.js";
 import admin from "../middleware/adminMiddlware.js";
 import auth from "../middleware/authMiddleware.js";
 
 router.get("/", getProducts);
-
 router.get("/:id", getProductById);
 
 /* ADMIN ACCSESS */
+router.post("/", [auth, admin], createProduct);
+router.put("/:id", [auth, admin], updateProduct);
 router.delete("/:id", [auth, admin], deleteProduct);
 
 export default router;
